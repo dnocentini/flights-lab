5c153bd4484a21e2da9fd5a28d27794ed8fa891e
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-21 20:59:39 -0600

    Created a Flight Model with airline, flightNo and departs properties

---
+++ models/flight.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+      type: String,
+      enum: ['American', 'Southwest', 'United']
+    },
+    flightNo: {
+      type: Number,
+      required: true,
+      max: 9999,
+      min: 10,
+    },
+    departs: {
+      type: Date,
+      default:  Date.now() + (366*24*60*60*1000)
+    }
+});      
