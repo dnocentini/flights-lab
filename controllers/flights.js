a97ac075ad08db37732518743f303a7ab5ea78c9
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-24 12:54:07 -0600

    Created detail/show view

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,11 +3,19 @@ var Flight = require('../models/flight');
 module.exports = {
   index,
   create,
-  new: newFlight
+  new: newFlight,
+  show
+};
+
+function show(req, res) {
+  Flight.findById(req.params.id, function (err, flight) {
+    console.log('FLIGHT: ', flight);
+    res.render('flights/show', { title: ` Flight # ${flight._id}`, flight });
+  });
 };
 
 function newFlight(req, res) {
-  res.render('flights/new', {title: 'New Flight'});
+  res.render('flights/new', { title: 'New Flight' });
 };
 
 function create(req, res) {
@@ -16,7 +24,7 @@ function create(req, res) {
   }
   var flight = new Flight(req.body);
   flight.save(function (err) {
-    if (err) return res.render('flights/new', {title: 'New Flight'});
+    if (err) return res.render('flights/new', { title: 'New Flight' });
     console.log(flight);
     res.redirect('flights');
   });
@@ -24,6 +32,6 @@ function create(req, res) {
 
 function index(req, res) {
   Flight.find({}, function (err, flights) {
-    res.render('flights', {title: 'All Flights', flights});
+    res.render('flights', { title: 'All Flights', flights });
   });
 };
